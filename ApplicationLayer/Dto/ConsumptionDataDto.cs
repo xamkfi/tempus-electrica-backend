e0aa046b4ea96eadcf971a4d75e2d65c22c118f4
Jetro Puranen <jetropuranenn@gmail.com>
2024-09-09 10:46:38 +0300

    Added FingridCalculationservice to db

---
+++ ApplicationLayer/Dto/ConsumptionDataDto.cs
@@ -0,0 +1,32 @@
+﻿namespace ApplicationLayer.Dto
+{
+    public class MonthlyConsumptionData
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public decimal Consumption { get; set; }
+        public decimal SpotPrice { get; set; }
+        public decimal FixedPrice { get; set; }
+    }
+
+    public class WeeklyConsumptionData
+    {
+        public int Year { get; set; }
+        public int Week { get; set; }
+        public decimal Consumption { get; set; }
+        public decimal SpotPrice { get; set; }
+        public decimal FixedPrice { get; set; }
+    }
+
+    public class DailyConsumptionData
+    {
+
+        public string? Day { get; set; }
+        public decimal Consumption { get; set; }
+        public decimal SpotPrice { get; set; }
+        public decimal FixedPrice { get; set; }
+
+
+
+    }
+}
