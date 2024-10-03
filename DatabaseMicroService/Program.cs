ba13c6cf3f94b632999a954a61d7ce514f56950b
jakulahtinen <jakulahtinen@msn.com>
2024-10-03 16:27:10 +0300

    Refactored ElectricityPriceService hard coded values to more accurate.

--- DatabaseMicroService/Program.cs
+++ DatabaseMicroService/Program.cs
@@ -46,11 +46,9 @@ public class Program
             .AddEnvironmentVariables()
             .AddUserSecrets<Program>(); // This adds user secrets
 
-
-        //var keyVaultManager = builder.Services.BuildServiceProvider().GetRequiredService<IKeyVaultSecretManager>();
-        //var vaultSecret = await keyVaultManager.GetSecretAsync();
-        var dbConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
-     
+        var keyVaultManager = builder.Services.BuildServiceProvider().GetRequiredService<IKeyVaultSecretManager>();
+        var vaultSecret = await keyVaultManager.GetSecretAsync();
+        var dbConnectionString = vaultSecret.DbConnectionString;
         // Register the DbContext with the connection string fetched from Key Vault
         builder.Services.AddDbContext<ElectricityDbContext>(options =>
             options.UseSqlServer(dbConnectionString));
